6e72262d08d68e605d5940f0de4606ad5e99ea34
Sorin  Cristescu <sorincristescu@Sorins-MacBook-Pro.local>
2020-09-21 05:40:35 +0200

    fetch all airports and populate table

--- src/views/Airports.vue
+++ src/views/Airports.vue
@@ -2,7 +2,7 @@
   <div class="airports">
     <v-data-table
       :headers="headers"
-      :items="desserts"
+      :items="allAirports"
       :items-per-page="10"
       :search="search"
       class="elevation-5 ma-5"
@@ -22,7 +22,7 @@
             hide-details
           ></v-text-field>
           <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="500px">
+          <v-dialog v-model="dialog" max-width="60%">
             <template v-slot:activator="{ on, attrs }">
               <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on"
                 >+ Add New Aiport</v-btn
@@ -36,35 +36,53 @@
               <v-card-text>
                 <v-container>
                   <v-row>
-                    <v-col cols="12" sm="6" md="4">
+                    <v-col cols="12" sm="4" md="4">
                       <v-text-field
+                        dense
+                        height="30px"
                         v-model="editedItem.name"
-                        label="Dessert name"
+                        label="Name"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.city"
+                        label="City"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.country"
+                        label="Country"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.iata"
+                        label="IATA"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.icao"
+                        label="ICAO"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.timezone"
+                        label="Timezone"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.tz"
+                        label="TZ"
                       ></v-text-field>
                     </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.calories"
-                        label="Calories"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.fat"
-                        label="Fat (g)"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.carbs"
-                        label="Carbs (g)"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.protein"
-                        label="Protein (g)"
-                      ></v-text-field>
+                    <v-col cols="12" sm="8" md="8">
+                      <v-row class="pl-10 pr-10">
+                        <p>Latitude:</p>
+                        <v-spacer></v-spacer>
+                        <p>Longitude:</p>
+                        <v-spacer></v-spacer>
+                        <p>Altitude:</p>
+                        <Map
+                          ref="map"
+                          apiKey="C12Se5f7UD7sOTcQ0MFSyw9zCJeO4i7qclGL6c-tTSE"
+                          latitude="37"
+                          longitude="-121"
+                          zoom="10"
+                        />
+                      </v-row>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -80,12 +98,29 @@
         </v-toolbar>
       </template>
       <template v-slot:[`item.actions`]="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)">
-          mdi-pencil
-        </v-icon>
-        <v-icon small @click="deleteItem(item)">
-          mdi-delete
-        </v-icon>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              small
+              class="mr-2"
+              @click="editItem(item)"
+              v-bind="attrs"
+              v-on="on"
+            >
+              mdi-pencil
+            </v-icon>
+          </template>
+          <span>Edit Airport</span>
+        </v-tooltip>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon small @click="deleteItem(item)" v-bind="attrs" v-on="on">
+              mdi-delete
+            </v-icon>
+          </template>
+          <span>Delete Airport</span>
+        </v-tooltip>
       </template>
       <template v-slot:no-data>
         <v-btn color="primary" @click="initialize">Reset</v-btn>
@@ -95,45 +130,63 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Airports",
   components: {
     // Map,
   },
   data: () => ({
+    search: "",
     dialog: false,
     headers: [
       {
-        text: "Dessert (100g serving)",
+        text: "Name",
         align: "start",
-        sortable: false,
         value: "name",
       },
-      { text: "Calories", value: "calories" },
-      { text: "Fat (g)", value: "fat" },
-      { text: "Carbs (g)", value: "carbs" },
-      { text: "Protein (g)", value: "protein" },
+      { text: "City", value: "city" },
+      { text: "Country", value: "country" },
+      { text: "IATA", value: "iata" },
+      { text: "ICAO", value: "icao" },
       { text: "Actions", value: "actions", sortable: false },
     ],
-    desserts: [],
+    airports: [],
     editedIndex: -1,
     editedItem: {
+      id: "",
       name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      city: "",
+      country: "",
+      iata: "",
+      icao: "",
+      DST: "U",
+      latitude: "",
+      longitude: "",
+      source: "OurAirports",
+      timezone: "",
+      type: "airport",
+      tz: "",
     },
     defaultItem: {
+      id: "",
       name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      city: "",
+      country: "",
+      iata: "",
+      icao: "",
+      DST: "U",
+      latitude: "",
+      longitude: "",
+      source: "OurAirports",
+      timezone: "",
+      type: "airport",
+      tz: "",
     },
   }),
 
   computed: {
+    ...mapGetters(["allAirports"]),
     formTitle() {
       return this.editedIndex === -1 ? "New Item" : "Edit Item";
     },
@@ -146,113 +199,50 @@ export default {
   },
 
   created() {
-    this.initialize();
+    this.getAllAirport();
   },
 
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-        },
-      ];
-    },
+    ...mapActions([
+      "getAllAirport",
+      "addNewAirport",
+      "updateAirport",
+      "deleteAirport",
+      "searchAirport",
+    ]),
+  },
 
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
+  initialize() {
+    this.getAllAirport();
+  },
 
-    deleteItem(item) {
-      const index = this.desserts.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.desserts.splice(index, 1);
-    },
+  editItem(item) {
+    this.editedIndex = this.desserts.indexOf(item);
+    this.editedItem = Object.assign({}, item);
+    this.dialog = true;
+  },
 
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
+  deleteItem(item) {
+    const index = this.desserts.indexOf(item);
+    confirm("Are you sure you want to delete this item?") &&
+      this.desserts.splice(index, 1);
+  },
 
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
-      } else {
-        this.desserts.push(this.editedItem);
-      }
-      this.close();
-    },
+  close() {
+    this.dialog = false;
+    this.$nextTick(() => {
+      this.editedItem = Object.assign({}, this.defaultItem);
+      this.editedIndex = -1;
+    });
+  },
+
+  save() {
+    if (this.editedIndex > -1) {
+      Object.assign(this.desserts[this.editedIndex], this.editedItem);
+    } else {
+      this.desserts.push(this.editedItem);
+    }
+    this.close();
   },
 };
 </script>
